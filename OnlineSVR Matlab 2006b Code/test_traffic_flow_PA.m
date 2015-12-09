ea25c0ff1312f15a5604375c2cfc37db5553eef1
Hsu-Chieh Hu <hsuchieh.hu@gmail.com>
2015-12-09 15:15:20 -0500

    output log file

--- OnlineSVR Matlab 2006b Code/test_traffic_flow_PA.m
+++ OnlineSVR Matlab 2006b Code/test_traffic_flow_PA.m
@@ -19,17 +19,28 @@ online_size = data_size - horizon - window_size + 1;%unit: sample_time
 
 PredictedY = zeros(1,online_size);
 ActualY = zeros(1,online_size);
+ActualX = zeros(online_size, window_size);
 Loss = zeros(1,online_size);
 weights = zeros(1,window_size);
 
 for i = 1:online_size
     OnlineSetX = series(i:i+window_size-1);%y1_avg(start+(i-1)*sample_time+1:sample_time:start+(i-1+window_size)*sample_time)';
+    ActualX(i,:) = OnlineSetX;
     PredictedY(i) = weights * OnlineSetX';
     OnlineSetY = mean(series(i+window_size:i+window_size+horizon-1));%mean(y1_avg(start+(i-1+window_size)*sample_time+1:start+(i-1+window_size)*sample_time+horizon)); %y1_avg(start+start_size+i+window_size+1);
     ActualY(i) = OnlineSetY;
     Loss(i) = pa_loss(weights,OnlineSetX, OnlineSetY, epsilon);
     weights = weights + sign(ActualY(i) - PredictedY(i)) * Loss(i) * OnlineSetX /sum((OnlineSetX).^2) ;  
 end
+
+fileID = fopen('test.txt','w');
+for i = 1:online_size
+    g=sprintf('%f ', ActualX(i,:));
+    formatSpec = '%f, %s\n';
+    fprintf(fileID,formatSpec,ActualY(i), g);
+end
+
+
 
 mean_sq_error = mean((PredictedY - ActualY).^2)
 figure;
