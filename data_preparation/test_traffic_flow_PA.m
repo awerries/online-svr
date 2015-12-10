0dc0158aa05d46617c64942bf7d3250a52ead58a
Hsu-Chieh Hu <hsuchieh.hu@gmail.com>
2015-12-10 17:24:49 -0500

    new log updates

--- data_preparation/test_traffic_flow_PA.m
+++ data_preparation/test_traffic_flow_PA.m
@@ -1,7 +1,7 @@
 load('traffic_data.mat')
 
 y1_avg = avg_arrival_rate(x1,y1);%preprocessing
-epsilon = 1;
+epsilon = 0.1;
 start = 312;
 sample_time = 30;%unit: second
 
@@ -13,8 +13,8 @@ for i = 1:data_size
 end
 
 
-window_size = 20*60/sample_time;%unit: sample_time
-horizon = 3*60/sample_time;%unit: sample_time
+window_size = 5*60/sample_time;%unit: sample_time
+horizon = 5*60/sample_time;%unit: sample_time
 online_size = data_size - window_size + 1;%unit: sample_time
 
 PredictedY = zeros(1,online_size);
@@ -32,7 +32,7 @@ for i = 1:online_size
         OnlineSetY = mean(series(i+window_size-horizon-1:i+window_size-1));
         ActualY(i-horizon) = OnlineSetY;
         Loss(i-horizon) = pa_loss(weights,ActualX(i-horizon,:), OnlineSetY, epsilon);
-        tau = Loss(i-horizon)/(sum((ActualX(i-horizon,:)).^2));
+        tau = Loss(i-horizon)/(sum((ActualX(i-horizon,:)).^2)+1/0.001);
         weights = weights + sign(ActualY(i-horizon) - PredictedY(i-horizon)) * ActualX(i-horizon,:) * tau ;
     end
 end
