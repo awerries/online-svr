e1e67735c9fcb468559a75aac4ae499508956f66
Hsu-Chieh Hu <hsuchieh.hu@gmail.com>
2015-12-11 12:16:25 -0500

    fix parser in a clean way and be able to generate log correctly

--- parse_surtrac.py
+++ parse_surtrac.py
@@ -4,6 +4,17 @@ import sys
 import copy
 
 
+all_phases = ['0','1']
+sample_rate = 30#how many seconds constitue a sample
+window_arrival = 10#1*60 / sample_rate
+predict_horizon = 5#window should be greater than horizon
+smooth_period = 60
+
+q_sample = Queue()
+q_feature = Queue()
+q_smooth = Queue()
+samples = list()
+
 def parse_line(line, all_phases):
 	field_size = 6
 	elements = line.split()
@@ -21,24 +32,15 @@ def arrival_rate(samples):
 	return np.mean(samples,0)
 
 
-def doLearning(line, q_sample, q_feature, q_smooth, samples, output_file, timestamp):
-	all_phases = ['0','1']
-	sample_rate = 30#how many seconds constitue a sample
-	window_arrival = 10#1*60 / sample_rate
-	predict_horizon = 5#window should be greater than horizon
-	smooth_period = 60
+def doLearning(line, output_file, timestamp):
 
-
-    
+ 
 	if len(samples) == sample_rate:
 		if q_sample.qsize() == window_arrival:
 			feature = np.array(q_sample.queue)
 			label = arrival_rate(feature[-predict_horizon:])
 			if q_feature.qsize() == predict_horizon:
 				previous_feature = q_feature.get()
-				#print "label ", label[0]
-				#print "previous_feature ", previous_feature[:,0].tolist()
-				#print "current_feature ", feature[:,0].tolist()
 				for pi, phase in enumerate(all_phases):
 					output_file[pi].write(str(timestamp)+": "+str(feature[:,pi].tolist())[1:-1]+": "+str(label[pi])+": "+str(previous_feature[:,pi].tolist())[1:-1]+"\n")
 				
@@ -46,7 +48,7 @@ def doLearning(line, q_sample, q_feature, q_smooth, samples, output_file, timest
 			q_sample.get()
 
 		q_sample.put(arrival_rate(samples))
-		del samples[:] #= list()
+		del samples[:]
 
 	if q_smooth.qsize() == smooth_period:
 		q_smooth.get()
@@ -57,10 +59,7 @@ def doLearning(line, q_sample, q_feature, q_smooth, samples, output_file, timest
 	
 	
 def main():
-	q_sample = Queue()
-	q_feature = Queue()
-	q_smooth = Queue()
-
+	
 	log_name = "./centre_graham_cluster_log.txt"
 	input_file = open(log_name,'r')
 	file_0 = open('phase0_10D_test.txt','w')
@@ -73,13 +72,6 @@ def main():
 	scale = 1000
 	dt = 1
 
-	samples = list()
-	'''all_phases = ['0','1']
-	sample_rate = 30#how many seconds constitue a sample
-	window_arrival = 10#1*60 / sample_rate
-	predict_horizon = 5#window should be greater than horizon
-	smooth_period = 60
-'''
 	for temp_line in input_file:
 
 		line = temp_line[0:-1]
@@ -91,54 +83,11 @@ def main():
 			for i in range(time_diff/scale - 1):
 				fake_msg_time += dt*scale
 				fake_msg = str(fake_msg_time)
-
-				#print fake_msg
-				doLearning(fake_msg, q_sample, q_feature, q_smooth, samples, output_file, fake_msg_time)
-				'''if len(samples) == sample_rate:
-					if q_sample.qsize() == window_arrival:
-						feature = np.array(q_sample.queue)
-						label = arrival_rate(feature[-predict_horizon:])
-						if q_feature.qsize() == predict_horizon:
-							previous_feature = q_feature.get()
-							for pi, phase in enumerate(all_phases):
-								output_file[pi].write(str(fake_msg_time)+": "+str(feature[:,pi].tolist())[1:-1]+": "+str(label[pi])+": "+str(previous_feature[:,pi].tolist())[1:-1]+"\n")
-						q_feature.put(feature)
-						q_sample.get()
-					q_sample.put(arrival_rate(samples))
-					samples = list()
-				if q_smooth.qsize() == smooth_period:
-					q_smooth.get()
-				q_smooth.put(parse_line(fake_msg, all_phases))
-				data = arrival_rate(list(q_smooth.queue))
-
-				samples.append(data)'''
-
-
+				doLearning(fake_msg, output_file, fake_msg_time)
 		elif time_diff == 0:
 			continue
-
-		#print line
-		doLearning(line, q_sample, q_feature, q_smooth, samples, output_file, new_msg_rcv_time)
 		
-		'''if len(samples) == sample_rate:
-			if q_sample.qsize() == window_arrival:
-				feature = np.array(q_sample.queue)
-				label = arrival_rate(feature[-predict_horizon:])
-				if q_feature.qsize() == predict_horizon:
-					previous_feature = q_feature.get()
-					for pi, phase in enumerate(all_phases):
-						output_file[pi].write(str(new_msg_rcv_time)+": "+str(feature[:,pi].tolist())[1:-1]+": "+str(label[pi])+": "+str(previous_feature[:,pi].tolist())[1:-1]+"\n")
-				q_feature.put(feature)
-				q_sample.get()
-			q_sample.put(arrival_rate(samples))
-			samples = list()
-
-		if q_smooth.qsize() == smooth_period:
-			q_smooth.get()
-		q_smooth.put(parse_line(line, all_phases))
-		data = arrival_rate(list(q_smooth.queue))
-		samples.append(data)'''
-		
+		doLearning(line, output_file, new_msg_rcv_time)	
 		prev_msg_rcv_time = new_msg_rcv_time
 		
 if __name__ == '__main__':
--- parse_surtrac_PA.py
+++ parse_surtrac_PA.py
@@ -3,6 +3,22 @@ import numpy as np
 import sys
 
 
+sample_rate = 30#how many seconds constitue a sample
+window_arrival = 5#1*60 / sample_rate
+predict_horizon = 4#window should be greater than horizon
+smooth_period = 60
+all_phases = ['0','1']
+epsilon = 1
+
+
+q_sample = Queue()
+q_feature = Queue()
+q_smooth = Queue()
+samples = list()
+weights = np.zeros((window_arrival,len(all_phases)))
+prediction = np.zeros(len(all_phases))
+
+
 def parse_line(line, all_phases):
 	field_size = 6
 	elements = line.split()
@@ -20,7 +36,6 @@ def arrival_rate(samples):
 	return np.mean(samples,0)
 
 def PA_loss(weights, answ, prev_feature):
-	epsilon = 0.1
 	loss = abs(np.inner(weights, prev_feature) -  answ)
 	if loss < epsilon:
 		loss = 0
@@ -36,62 +51,76 @@ def sign(x):
         return -1
 
 
+def PA_learning(line, output_file, timestamp, weights):
+
+
+	if len(samples) == sample_rate:
+		if q_sample.qsize() == window_arrival:
+			feature = np.array(q_sample.queue)
+			answ = arrival_rate(feature[-predict_horizon:])
+			if q_feature.qsize() == predict_horizon:
+				prev_prediction, prev_feature = q_feature.get()
+			
+				for pi, phase in enumerate(all_phases):
+					prediction[pi] = np.inner(weights[:,pi], feature[:,pi])
+					output_file[pi].write(str(timestamp)+": "+str(prediction[pi])+"\n")
+					if prediction[pi] < 0.0:
+						prediction[pi]  = 0.0
+					if np.linalg.norm(prev_feature[:,pi]) == 0.0:
+						continue
+					loss = PA_loss(weights[:,pi], answ[pi], prev_feature[:,pi])
+					tau = loss/((np.linalg.norm(prev_feature[:,pi]))**2+1/0.2)
+					weights[:,pi]  = weights[:,pi] + sign(answ[pi] - prev_prediction[pi]) * prev_feature[:,pi] * tau
+					#output_file[pi].write(str(prev_prediction[pi])+" "+str(answ[pi])+'\n')
+
+			
+			q_feature.put([prediction, feature])
+			q_sample.get()
+
+		q_sample.put(arrival_rate(samples))
+		del samples[:]
+
+	if q_smooth.qsize() == smooth_period:
+		q_smooth.get()
+	q_smooth.put(parse_line(line, all_phases))
+	data = arrival_rate(list(q_smooth.queue))
+
+	samples.append(data)
+
+
+
 def main():
-	q_sample = Queue()
-	q_feature = Queue()
-	q_smooth = Queue()
-
-	log_name = "../input_log.txt"
+	log_name = "./centre_graham_cluster_log.txt"
 	input_file = open(log_name,'r')
-	all_phases = ['0','1']
+	file_0 = open('phase0_5D_PA.txt','w')
+	file_1 = open('phase1_5D_PA.txt','w')
+	output_file =[file_0, file_1]
 
-	sample_rate = 1#how many seconds constitue a sample
-	samples = list()
-	window_arrival = 4#1*60 / sample_rate
-	predict_horizon = 2#window should be greater than horizon
-	smooth_period = 10
 
-	weights = np.zeros((window_arrival,len(all_phases)))
-	prediction = np.zeros(len(all_phases))
+	new_msg_rcv_time = 0
+	prev_msg_rcv_time = 0
+	scale = 1000
+	dt = 1
 
-	for line in input_file:
-	#while True:
-	#	line = sys.stdin.readline()
-		if len(samples) == sample_rate:
-			if q_sample.qsize() == window_arrival:
-				feature = np.array(q_sample.queue)
-				answ = arrival_rate(feature[-predict_horizon:])
-				if q_feature.qsize() == predict_horizon:
-					prev_prediction, prev_feature = q_feature.get()
-					
-					#print "answ ", answ[0], answ[1]
-					#print "prev_feature ", prev_feature[:,0].tolist(),prev_feature[:,1].tolist()
-					#print "current_feature ", feature[:,0].tolist(), feature[:,1].tolist()
+	for temp_line in input_file:
 
-					for pi, phase in enumerate(all_phases):
-						prediction[pi] = np.inner(weights[:,pi], feature[:,pi])
-						if prediction[pi] < 0:
-							prediction[pi]
-						if np.linalg.norm(prev_feature[:,pi]) == 0.0:
-							continue
-						loss = PA_loss(weights[:,pi], answ[pi], prev_feature[:,pi])
-						tau = loss/(np.linalg.norm(prev_feature[:,pi]))**2
-						weights[:,pi]  = weights[:,pi] + sign(answ[pi] - prev_prediction[pi]) * prev_feature[:,pi] * tau
-					print prev_prediction, answ
+		line = temp_line[0:-1]
 
-				
-				q_feature.put([prediction, feature])
-				q_sample.get()
+		new_msg_rcv_time = int(line.split()[0])
+		time_diff = new_msg_rcv_time - prev_msg_rcv_time
+		if  time_diff/scale > dt:
+			fake_msg_time = prev_msg_rcv_time
+			for i in range(time_diff/scale - 1):
+				fake_msg_time += dt*scale
+				fake_msg = str(fake_msg_time)
+				PA_learning(fake_msg, output_file, fake_msg_time, weights)
+		elif time_diff == 0:
+			continue
+		
+		PA_learning(line, output_file, new_msg_rcv_time, weights)	
+		prev_msg_rcv_time = new_msg_rcv_time
+		
 
-			q_sample.put(arrival_rate(samples))
-			samples = list()
-
-		if q_smooth.qsize() == smooth_period:
-			q_smooth.get()
-		q_smooth.put(parse_line(line, all_phases))
-		data = arrival_rate(list(q_smooth.queue))
-
-		samples.append(data)
 
 		
 
